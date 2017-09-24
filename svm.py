a91f961e9b1f784d2b1cf145b1c34da24d53b061
AFAgarap <abien.fred.agarap@adamson.edu.ph>
2017-09-24 19:03:02 +0800

    Finished writing the method for training the model. In train(), there is a validation at the end, and display of confusion matrix

--- svm.py
+++ svm.py
@@ -20,11 +20,14 @@ from __future__ import print_function
 __version__ = '0.1'
 __author__ = 'Abien Fred Agarap'
 
-import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
 import sys
 import tensorflow as tf
+import time
 
-BATCH_SIZE = 50
+BATCH_SIZE = 40
+LEARNING_RATE = 1e-5
 NUM_CLASSES = 2
 
 
@@ -45,17 +48,20 @@ class SVM:
                 x_input = tf.placeholder(dtype=tf.float32, shape=[None, self.num_features], name='x_input')
 
                 # [BATCH_SIZE]
-                y_input = tf.placeholder(dtype=tf.float32, shape=[None, 1], name='y_input')
+                y_input = tf.placeholder(dtype=tf.uint8, shape=[None], name='y_input')
+
+                # [BATCH_SIZE, NUM_CLASSES]
+                y_onehot = tf.one_hot(y_input, 2, on_value=1, off_value=-1, name='y_onehot')
 
             learning_rate = tf.placeholder(dtype=tf.float32, name='learning_rate')
 
             with tf.name_scope('training_ops'):
                 with tf.name_scope('weights'):
                     weight = tf.get_variable(name='weights',
-                                             initializer=tf.random_normal([self.num_features, 1], stddev=0.01))
+                                             initializer=tf.random_normal([self.num_features, NUM_CLASSES], stddev=0.01))
                     self.variable_summaries(weight)
                 with tf.name_scope('biases'):
-                    bias = tf.get_variable(name='biases', initializer=tf.constant([0.1]))
+                    bias = tf.get_variable(name='biases', initializer=tf.constant([0.1], shape=[NUM_CLASSES]))
                     self.variable_summaries(bias)
                 with tf.name_scope('Wx_plus_b'):
                     output = tf.matmul(x_input, weight) + bias
@@ -64,7 +70,8 @@ class SVM:
             with tf.name_scope('svm'):
                 regularization = 0.5 * tf.reduce_sum(tf.square(weight))
                 hinge_loss = tf.reduce_sum(
-                    tf.square(tf.maximum(tf.zeros([BATCH_SIZE, NUM_CLASSES]), 1 - y_input * output)))
+                    tf.square(tf.maximum(tf.zeros([BATCH_SIZE, NUM_CLASSES]),
+                                         1 - tf.cast(y_onehot, tf.float32) * output)))
                 with tf.name_scope('loss'):
                     loss = regularization + self.svm_c * hinge_loss
             tf.summary.scalar('loss', loss)
@@ -75,7 +82,7 @@ class SVM:
                 predicted_class = tf.sign(output)
                 predicted_class = tf.identity(predicted_class, name='prediction')
                 with tf.name_scope('correct_prediction'):
-                    correct = tf.equal(tf.argmax(predicted_class, 1), tf.argmax(y_input, 1))
+                    correct = tf.equal(tf.argmax(predicted_class, 1), tf.argmax(y_onehot, 1))
                 with tf.name_scope('accuracy'):
                     accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
             tf.summary.scalar('accuracy', accuracy)
@@ -84,19 +91,69 @@ class SVM:
 
             self.x_input = x_input
             self.y_input = y_input
+            self.y_onehot = y_onehot
             self.learning_rate = learning_rate
             self.loss = loss
             self.optimizer = optimizer
+            self.output = output
             self.predicted_class = predicted_class
             self.accuracy = accuracy
             self.merged = merged
 
-        sys.stdout.write('\n <log> Building graph...')
+        sys.stdout.write('\n<log> Building graph...')
         __graph__()
-        sys.stdout.write('</log> \n')
+        sys.stdout.write('</log>\n')
 
-    def train(self, train_data):
-        pass
+    def train(self, train_data, train_size, validation_data):
+        """Trains the model"""
+
+        # initialize the variables
+        init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+
+        # get the current time and date
+        timestamp = str(time.asctime())
+
+        # event files to contain the TensorBoard log
+        train_writer = tf.summary.FileWriter(self.log_path + timestamp + '-training', graph=tf.get_default_graph())
+
+        with tf.Session() as sess:
+            sess.run(init_op)
+
+            try:
+                for step in range(self.num_epochs * train_size // BATCH_SIZE):
+                    offset = (step * BATCH_SIZE) % train_size
+                    batch_train_data = train_data[0][offset:(offset + BATCH_SIZE)]
+                    batch_train_labels = train_data[1][offset:(offset + BATCH_SIZE)]
+
+                    feed_dict = {self.x_input: batch_train_data, self.y_input: batch_train_labels,
+                                 self.learning_rate: LEARNING_RATE}
+
+                    summary, _, step_loss = sess.run([self.merged, self.optimizer, self.loss], feed_dict=feed_dict)
+
+                    if step % 100 == 0:
+                        train_accuracy = sess.run(self.accuracy, feed_dict=feed_dict)
+                        print('step[{}] train -- loss : {}, accuracy : {}'.format(step, step_loss, train_accuracy))
+                        train_writer.add_summary(summary=summary, global_step=step)
+            except KeyboardInterrupt:
+                print('Training interrupted at step {}'.format(step))
+            finally:
+                print('EOF -- training done at step {}'.format(step))
+
+                feed_dict = {self.x_input: validation_data[0][:BATCH_SIZE],
+                             self.y_input: validation_data[1][:BATCH_SIZE]}
+
+                validation_accuracy = sess.run(self.accuracy, feed_dict=feed_dict)
+
+                print('Validation accuracy : {}'.format(validation_accuracy))
+
+                predicted_labels = sess.run(self.predicted_class,
+                                            feed_dict={self.x_input: validation_data[0][:BATCH_SIZE]})
+
+                predicted_labels = sess.run(tf.maximum(predicted_labels[:, 0], predicted_labels[:, 1]))
+
+                conf = confusion_matrix(validation_data[1][:BATCH_SIZE], predicted_labels)
+                print('Confusion matrix : {}'.format(conf))
+                plt.imshow(conf, cmap='binary', interpolation='None').show()
 
     @staticmethod
     def variable_summaries(var):
